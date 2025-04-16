7b707f8cd31ed9086d48341edf33c7e29a663750
prethith <prathithchivukula@gmail.com>
2025-04-16 10:52:38 +0530

    Styled home page. fixed mobile styling issues

--- layouts/base.11ty.js
+++ layouts/base.11ty.js
@@ -4,8 +4,10 @@ export default function base(data) {
   const isHomePage = page.url === '/' || page.url === '/index/' || page.url === '/index.html';
   const mainContent = isHomePage ? `
     <div class="main-content">
+     <section class="intro">
       <h1>Tear it all apart. See what's inside.</h1>
       <p>Every sensationalist statistic or story you hear is a carefully crafted illusion, designed to shape a narrative by obscuring its constituent elements — which are often the most crucial details. Our mission is to tear it all apart, expose the hidden pieces, and strip away the sensationalism to reveal what is real.</p>
+      </section>
       <section class="blog-posts">
         <h2>latest posts</h2>
         <ul>${content}</ul>
@@ -22,7 +24,9 @@ export default function base(data) {
       <h2>about</h2>
       <p>a blog by prathith chivukula.</p>
       <h2>links</h2>
-      <a href="https://www.github.com/prethith">GitHub</a>
+      <ul class=links>
+        <li><a href="https://www.github.com/prethith">GitHub</a></li>
+      </ul>
       <h2>categories</h2>
       <ul>
         ${collections && collections.categories ? 
